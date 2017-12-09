8f3cf576d7915085873e645314d91ed3ebe54b1d
curder <curder@foxmail.com>
2017-12-09 19:00:14 +0800

    获取oauth_token和用户登录

--- client_labs/src/App.vue
+++ client_labs/src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div id="home-wrapper" class="wrapper">
+    <section class="login">
+      <div class="row">
+        <div class="col-md-6 col-md-push-3">
+          <div class="panel panel-default">
+            <div class="panel-heading"><strong>登录</strong></div>
+            <div class="panel-body">
+              <form @submit.prevent="handleLoginFormSubmit()">
+                <div class="form-group">
+                  <label for="email">Email地址</label>
+                  <input
+                  placeholder="请输入您的邮箱地址"
+                  v-model="login.email"
+                  class="form-control"
+                  type="text" >
+              </div>
+
+              <div class="form-group">
+                <label for="password">密码</label>
+                <input
+                placeholder="请输入您的密码"
+                v-model="login.password"
+                class="form-control"
+                type="password">
+              </div>
+
+              <button class="btn btn-primary">登录</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import {loginUrl} from './config'
+import {clientId, clientSecret} from './env'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      login: {
+        email: 'test@admin.com',
+        password: 'password'
+      }
+    }
+  },
+  methods: {
+    handleLoginFormSubmit() {
+      const postData = {
+        grant_type: 'password',
+        client_id: clientId,
+        client_secret: clientSecret,
+        scope: '',
+        username: this.login.email,
+        password: this.login.password,
+      }
+      axios.post(loginUrl, postData)
+        .then(response => {
+          console.log(response)
+        })
+        ;
+    }
+  },
+
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "./assets/sass/app.scss"
 </style>
--- client_labs/src/main.js
+++ client_labs/src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  render: h => h(App)
+window.axios = axios
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/', component: App, name: 'home'},
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
 })
+
+new Vue({
+  router,
+}).$mount("#app")

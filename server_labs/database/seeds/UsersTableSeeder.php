5979486a2c57d0bd07792c34b19327c9b2d5caa7
curder <curder@foxmail.com>
2017-12-09 17:16:18 +0800

    添加一个测试用户

---
+++ server_labs/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user1 = [
+            'name' => 'Test',
+            'email' => 'test@admin.com',
+            'password' => Hash::make('password'),
+        ];
+
+        factory(App\User::class)->create($user1);
+
+    }
+}
